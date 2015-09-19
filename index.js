aedcca9af9d3411829c16040b21ae22f8c5f891f
Karissa McKelvey <krmckelv@gmail.com>
2015-09-19 15:21:13 -0700

    ractive toolkit init

---
+++ index.js
@@ -0,0 +1,13 @@
+var Ractive = require('ractive')
+var prettyBytes = require('pretty-bytes')
+var relativeDate = require('relative-date')
+
+var templateHelpers = Ractive.defaults.data
+
+templateHelpers.prettyBytes = function (bytes) {
+  return prettyBytes(bytes)
+}
+
+templateHelpers.relativeDate = function (iso) {
+  return relativeDate(new Date(iso))
+}
